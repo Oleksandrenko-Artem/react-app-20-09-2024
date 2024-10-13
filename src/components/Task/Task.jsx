c36f6886cf8d4cc1a29c8b44df71e185fc2cfb59
Artem Oleksandrenko <artem2008.zp@gmail.com>
2024-10-13 20:29:44 +0300

    Refactor Task

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 
 const Task = (props) => {
-    const {text, days, isDone} = props;
+    const {dataTask: {text, days, isDone}} = props;
     return (
         <div>
             <p style={{color: isDone ? 'green' : 'red'}}>{text}</p>
@@ -13,9 +13,11 @@ const Task = (props) => {
 
 
 Task.propTypes = {
-    text: PropTypes.string.isRequired,
-    days: PropTypes.number.isRequired,
-    isDone: PropTypes.bool
+    dataTask: PropTypes.shape({
+        id: PropTypes.number,
+        text: PropTypes.string.isRequired,
+        days: PropTypes.number.isRequired,
+        isDone: PropTypes.bool})
 };
 
 
