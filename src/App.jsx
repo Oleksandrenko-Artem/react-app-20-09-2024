2aa18b607b798301f8bea73951ae0642b7d3bbac
Artem Oleksandrenko <artem2008.zp@gmail.com>
2024-12-31 00:28:35 +0200

    Add Wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import TasksList from './components/TasksList/TasksList';
+
+import Wrapper from './components/Wrapper/Wrapper';
+import Timer from './components/Timer/Timer';
 
 function App() {
   
   return (
     <>
-      <TasksList/>
+      <Wrapper>
+        <Timer/>
+      </Wrapper>
+      <Wrapper>
+        <h2>Hi!</h2>
+      </Wrapper>
+      <Wrapper>
+        <h2>Hello!</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit architecto modi nulla eveniet quasi aperiam. Asperiores omnis ea voluptatum facilis.</p>
+      </Wrapper>
     </>
   )
 }
---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import styles from './Wrapper.module.scss';
+
+
+const Wrapper = (props) => {
+    const { children } = props;
+    return (
+        <div className={styles.wrapper}>
+            {children}
+        </div>
+    );
+};
+
+
+Wrapper.propTypes = {
+    children: PropTypes.any
+};
+
+
+export default Wrapper;
