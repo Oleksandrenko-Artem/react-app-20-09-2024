549e71309f0ce286dc8b8521a6652df6d0d16b03
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-01-21 17:50:11 +0200

    Add MouseTracker

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,13 @@
+import MouseTracker from "../components/MouseTracker/MouseTracker";
 
 
 const AboutPage = () => {
     return (
         <section>
             <h1>About page</h1>
+            <MouseTracker render={(position) => (<h3>x={position.x}, y={position.y}</h3>)} />
+            <hr />
+            <MouseTracker render={(position)=>(<div style={{position: 'absolute', top: position.y, left: position.x, pointerEvents: 'none'}}>+</div>)}/>
         </section>
     );
 }
