aecccccab994404d84e9d78d6e45fa68c4ed039b
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-01-21 18:41:12 +0200

    Add Slider

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,15 +1,17 @@
-import MouseTracker from "../components/MouseTracker/MouseTracker";
-
+import Slider from './../components/Slider/Slider';
 
 const AboutPage = () => {
     return (
         <section>
             <h1>About page</h1>
-            <MouseTracker render={(position) => (<h3>x={position.x}, y={position.y}</h3>)} />
-            <hr />
-            <MouseTracker render={(position)=>(<div style={{position: 'absolute', top: position.y, left: position.x, pointerEvents: 'none'}}>+</div>)}/>
+            <Slider min={0} max={1000} render={(value) => (<p>current value = {value}</p>)} />
+            <Slider min={0} max={100} render={(value) => (
+                <div style={{height: '50px', backgroundColor: 'lightblue'}}>
+                <div style={{height: '100%', backgroundColor: 'purple', width: `${value}%`}}>
+                </div>
+            </div>)} />
         </section>
     );
-}
+};
 
 export default AboutPage;
