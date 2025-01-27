ab8f6382fe65ec210685018fe5ac872c8a814bf1
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-01-27 18:10:48 +0200

    Add Counter

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,24 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../../store/counterSlice';
+
+const Counter = () => {
+    const countValue = useSelector((state) => state.counter.count);
+    const dispatch = useDispatch();
+    const addCount = () => {
+        dispatch(increment());
+    }
+    const subCount = () => {
+        dispatch(decrement());
+    }
+    return (
+        <div>
+            <h2>Counter {countValue}</h2>
+            <div>
+                <button onClick={addCount}>+</button>
+                <button onClick={subCount}>-</button>
+            </div>
+        </div>
+    );
+}
+
+export default Counter;
